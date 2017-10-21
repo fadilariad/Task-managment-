a60bfbf9abdf7d20cb6c2694aecbca0160ba2b96
fadilariad <32985210+fadilariad@users.noreply.github.com>
2017-10-21 19:41:12 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,8 @@
 <?php
 
- //  $DB_SERVER = "localhost:3306";
- //  $DB_USERNAME = "root";
- //  $DB_PASSWORD = "root";
-
-   $DB_SERVER = "localhost:3306";
-   $DB_USERNAME = "riad";
-   $DB_PASSWORD = "321riad123";
-
-//   $DB_SERVER = "31.154.95.29:3306";
-//   $DB_USERNAME = "riad";
-//   $DB_PASSWORD = "321riad123";
- 
+  $DB_SERVER = "localhost:3306";
+  $DB_USERNAME = "root";
+   $DB_PASSWORD = "root";
    $DB_DATABASE = "tasks";
    $db = mysqli_connect($DB_SERVER,$DB_USERNAME,$DB_PASSWORD,$DB_DATABASE);
    $db -> set_charset("utf8");
